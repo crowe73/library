1c2883ee369f3cd260886de1f14c6d14d5f17687
crowe73 <rowechad73@yahoo.com>
2022-07-02 22:40:34 -0500

    Add basic layout and functionality of page
    Add displayBooksOnPage() function.
    Add function calls to manually add  display books to library.
    Add css rules to get basic layout of page.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 # Development Log
 
-06/23/2022 23:27 - Project setup.
+6/23/2022 23:27 - Project setup.
 
-06/23/2022 23:55 - Declare empty array and create addBookToLibrary() function.
+6/23/2022 23:55 - Declare empty array and create addBookToLibrary() function.
+
+7/02/2022 21:30 - Add basic layout and functionality of page.
--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-// Declare Declare empty array.
+// Declare empty array.
 let myLibrary = [];
 
 // Object Constructor.
@@ -8,17 +8,52 @@ function Book(title, author, pages, read)
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function()
-    {
-        return(title + author + ", " + pages + ", " + read);
-    }
+    //this.info = function()
+    //{
+        //return(title + author + ", " + pages + ", " + read);
+    //}
 }
 
 // Add a book to library.
-function addBookToLibrary()
+function addBookToLibrary(title, author, pages, read)
 {
     // Add a book.
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
 }
 
-const theHobbit = new Book("The Hobbit", " by J.R.R Tolkien", " 295 pages", " not read yet");
-console.log(theHobbit.info());
+//const theHobbit = new Book("The Hobbit", " by J.R.R Tolkien", " 295 pages", " not read yet");
+//console.log(theHobbit.info());
+
+// Display the library array to cards.
+function displayBooksOnPage()
+{
+    const books = document.querySelector(".books");
+
+    // Loop over the library array and display to cards.
+    myLibrary.forEach(myLibrary => 
+        {
+            const card = document.createElement("div");
+            card.classList.add("card");
+            books.appendChild(card);
+            for (let key in myLibrary)
+            {
+                console.log(`${key}: ${myLibrary[key]}`);
+                const paragraph = document.createElement("p");
+                paragraph.textContent = (`${key}: ${myLibrary[key]}`);
+                card.appendChild(paragraph);
+            }
+        })
+}
+
+// Function calls to add books manually until form is built.
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", "295 pages", "not read yet");
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", "295 pages", "not read yet");
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", "295 pages", "not read yet");
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", "295 pages", "not read yet");
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", "295 pages", "not read yet");
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", "295 pages", "not read yet");
+
+console.log("End of code array contents", myLibrary);
+
+displayBooksOnPage();
